cab98b334968b1fbb8e63af402805e863d6a85e3
marmooo <marmooo@users.noreply.github.com>
2022-05-03 19:21:06 +0900

    update db

--- reduce.py
+++ reduce.py
@@ -10,8 +10,15 @@ with open("words.lst", "w") as fout:
             words[word] = True
             fout.write(word + "\n")
 
+count = 0
+with open("cc.zh.300.vec") as f:
+    for line in f:
+        word = line.split(" ", 1)[0]
+        if word in words:
+            count += 1
+
 with open("cc.zh.300-small.vec", "w") as fout:
-    fout.write(str(len(words)) + " 300\n")
+    fout.write(str(count) + " 300\n")
     with open("cc.zh.300.vec") as fin:
         for line in fin:
             word = line.split(" ", 1)[0]

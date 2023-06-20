efd004f5453ced9493d28762a73d1da2a680c6b4
marmooo <marmooo@users.noreply.github.com>
2023-06-20 11:05:09 +0900

    use deno_emit

--- build.js
+++ build.js
@@ -1,26 +1,5 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
-import { DB } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
+import { bundle } from "https://deno.land/x/emit/mod.ts";
 
-const remoteDB = new DB("remote.db");
-remoteDB.query("pragma synchronouse=OFF");
-remoteDB.query("pragma journal_mode=WAL");
-remoteDB.query(`
-  CREATE TABLE IF NOT EXISTS siminyms (
-    lemma TEXT,
-    words TEXT
-  )
-`);
-const insertCollocation = remoteDB.prepareQuery(`
-  INSERT INTO siminyms (lemma, words) VALUES(?, ?);
-`);
-
-const fileReader = await Deno.open("siminyms.tsv");
-remoteDB.query("begin");
-for await (const line of readLines(fileReader)) {
-  const [lemma, siminyms] = line.split("\t");
-  insertCollocation.execute([lemma, siminyms]);
-}
-remoteDB.query("commit");
-remoteDB.query(`
-  CREATE INDEX IF NOT EXISTS siminyms_index ON siminyms(lemma)
-`);
+const url = new URL(import.meta.resolve(Deno.args[0]));
+const { code } = await bundle(url);
+console.log(code);

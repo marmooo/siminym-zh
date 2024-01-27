9451b6049c0d13aa0633f1dfac7e5de16db0385f
marmooo <marmooo@users.noreply.github.com>
2024-01-28 00:01:06 +0900

    update

--- build-db.sh
+++ build-db.sh
@@ -2,7 +2,7 @@ size=$1
 
 rm remote.db
 rm -rf docs/db/$size
-deno run -A --unstable build-db.js
+deno run -A --unstable-ffi build-db.js
 bash optimize.sh
 mkdir -p docs/db/$size
 bash create_db.sh remote.db docs/db/$size

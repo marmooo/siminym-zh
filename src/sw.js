1afe9a83666a48c02e89ce65ac1aed223f268485
marmooo <marmooo@users.noreply.github.com>
2025-12-30 01:19:27 +0900

    improve service worker

--- src/sw.js
+++ src/sw.js
@@ -1,35 +1,40 @@
-const CACHE_NAME = "2025-09-09 00:19";
+const cacheName = "2025-12-30 00:00";
 const urlsToCache = [
-  "/siminym-zh/",
   "/siminym-zh/index.js",
   "/siminym-zh/sql.js-httpvfs/sql-wasm.wasm",
   "/siminym-zh/sql.js-httpvfs/sqlite.worker.js",
   "/siminym-zh/favicon/favicon.svg",
 ];
 
+async function preCache() {
+  const cache = await caches.open(cacheName);
+  await Promise.all(
+    urlsToCache.map((url) =>
+      cache.add(url).catch((err) => console.warn("Failed to cache", url, err))
+    ),
+  );
+  self.skipWaiting();
+}
+
+async function handleFetch(event) {
+  const cached = await caches.match(event.request);
+  return cached || fetch(event.request);
+}
+
+async function cleanOldCaches() {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames.map((name) => name !== cacheName ? caches.delete(name) : null),
+  );
+  self.clients.claim();
+}
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }),
-  );
+  event.waitUntil(preCache());
 });
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
+  event.respondWith(handleFetch(event));
 });
-
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName)),
-      );
-    }),
-  );
+  event.waitUntil(cleanOldCaches());
 });
